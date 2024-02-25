bef0e8263028c12e1ad7cc28eb5ece1c2bc5cb01
bbfodor <fodor.balint97@gmail.com>
2024-02-25 18:07:52 +0100

    feat: implement pdf upload

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -37,6 +37,25 @@ export const appRouter = router({
       },
     });
   }),
+  getfile: privateProcedure
+    .input(
+      z.object({
+        key: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      });
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' });
+
+      return file;
+    }),
   deleteFile: privateProcedure
     .input(
       z.object({
