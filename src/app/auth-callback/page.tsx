15b02b7a75ab422eeee4f6423dac5648e0cffae6
bbfodor <fodor.balint97@gmail.com>
2024-03-05 23:23:39 +0100

    misc: enable missing suspense exp ff

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,7 +2,6 @@
 
 import { Loader2 } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
 import { trpc } from '../_trpc/client';
 
 const Page = () => {
@@ -19,16 +18,14 @@ const Page = () => {
   });
 
   return (
-    <Suspense fallback={<Loader2 className='h-8 w-8 animate-spin text-zinc-800' />}>
-      <div className='w-full mt-24 flex justify-center'>
-        <div className='flex flex-col items-center gap-2'>
-          <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
 
-          <h3 className='font-semibold text-xl'>Setting up your account...</h3>
-          <p>You will be redirected automatically.</p>
-        </div>
+        <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
